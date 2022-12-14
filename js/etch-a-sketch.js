492855e58915da58677518d055003ea62ac79904
itsbbgawara <itsbbgawara@gmail.com>
2022-12-14 19:51:56 +0700

    update canvas function and remove canvas grid

--- js/etch-a-sketch.js
+++ js/etch-a-sketch.js
@@ -15,10 +15,81 @@ function createCanvas(newSize) {
     for (i = 1; i <= newSize * newSize; i++) {
         const createGrid = document.createElement('div');
         createGrid.classList.add('cell');
-        createGrid.style.border = "0.5px solid rgb(200, 200, 200)";
+        //createGrid.style.border = "0.5px solid rgb(200, 200, 200)";
         createGrid.style.backgroundColor = 'white';
         canvas.appendChild(createGrid);
     }
     canvas.addEventListener('click', getColor);
 };
 createCanvas(16);
+
+function getColor() {
+    const cells = document.querySelectorAll('.cell');
+
+    if (!currentState) {
+        cells.forEach((cell) => {
+            cell.addEventListener('mouseleave', newColor);
+        });
+        currentState = true;
+    } else {
+        cells.forEach((cell) => {
+            cell.removeEventListener('mouseleave', newColor);
+        });
+        currentState = false;
+    }
+};
+
+defaultBtn.addEventListener('click', function () {
+    color = 'default';
+});
+
+randomsBtn.addEventListener('click', function () {
+    color = 'randoms';
+})
+
+eraserBtn.addEventListener('click', function () {
+    color = 'eraser';
+});
+
+resetBtn.addEventListener('click', function () {
+    color = 'reset';
+})
+
+function newColor(e) {
+    switch (color) {
+        case 'randoms' :
+            let randomColor = Math.floor(Math.random() * 360);
+            pickColor = `hsl(${randomColor}, 100%, 50%)`;
+            e.target.style.backgroundColor = pickColor;
+            break;
+        case 'default':
+            pickColor = 'black';
+            e.target.style.backgroundColor = pickColor;
+            break;
+        case 'eraser':
+            pickColor = 'white';
+            e.target.style.backgroundColor = pickColor;
+            break;
+        case 'reset':
+            pickColor = 'white';
+            e.target.style.backgroundColor = pickColor;
+            break;
+    };
+}
+
+function newGridSize() {
+    let newGrid = document.querySelectorAll('.cell');
+    newGrid.forEach((div) => {
+        return div.remove();
+    });
+    createCanvas(gridSize.value);
+}
+
+gridSize.addEventListener('mouseup', newGridSize);
+
+resetBtn.addEventListener('click', function () {
+    let newGrid = document.querySelectorAll('.cell');
+    newGrid.forEach((cell) => {
+        cell.style.backgroundColor = 'white';
+    });
+});
